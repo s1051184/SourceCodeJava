0c5e71565dd051d6b97210f76beea52f6d640ba6
Patcharaporn <ms_bullshitta@hotmail.com>
2012-11-23 15:39:13 +0700

    Add subtraction

--- TestSVN/src/TestAddition.java
+++ TestSVN/src/TestAddition.java
@@ -10,13 +10,18 @@ public class TestAddition {
 		System.out.print("Start Program");
 		int a= Integer.parseInt(JOptionPane.showInputDialog("Enter 1st number"));
 		int b= Integer.parseInt(JOptionPane.showInputDialog("Enter 2nd number"));
-		int result = Addition(a,b);
-		JOptionPane.showMessageDialog(null, "The result is "+result);
+		int resultAddition = Addition(a,b);
+		int resultSubtraction = Subtration(a,b);
+		JOptionPane.showMessageDialog(null, "The result is \n Addition : "+resultAddition+"\n Subtraction : "+resultSubtraction);
 		System.out.print(a);
 	}
 	public static int Addition(int a,int b){
 		System.out.print("Adding Number");
 		return a+b;
+	}
+	public static int Subtration(int a,int b){
+		System.out.print("Adding Number");
+		return a-b;
 	}
 
 }
